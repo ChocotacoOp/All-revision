bf66178a93656cd715eacec2bfb0da3adbcbd3ad
unknown <rajatsantra8116@gmail.com>
2022-11-21 15:26:24 +0530

    day15

---
+++ SpringBootRestapp2/src/main/java/com/masai/SpringBootRestapp2Application.java
@@ -0,0 +1,13 @@
+package com.masai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestapp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestapp2Application.class, args);
+	}
+
+}
---
+++ SpringBootRestapp2/src/main/java/com/masai/controller/StudentController.java
@@ -0,0 +1,112 @@
+package com.masai.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.masai.model.Student;
+
+@RestController
+public class StudentController {
+
+	private List<Student> students = new ArrayList<>();
+	
+	public StudentController(){
+		
+		students.add(new Student(10, "rajat", 800));
+		students.add(new Student(20, "rahul", 850));
+		students.add(new Student(30, "kousik", 880));
+		
+	}
+	
+	@GetMapping("/students")
+	public List<Student> getAllStudenthandler(){
+		
+		return students;
+	}
+	
+	@GetMapping("/students/{roll}")
+	public Student getStudentbyRollHandler(@PathVariable("roll") Integer roll) {
+		
+		List<Student> list= students.stream().filter(s-> s.getRoll()==roll).collect(Collectors.toList());
+		
+		if(list.size() ==0)
+			throw new IllegalArgumentException("student does not exist with roll"+roll);
+		
+		return list.get(0);
+	}
+	
+	@PostMapping("/students")
+	public List<Student> addStudentHandler(@RequestBody Student student){
+		
+		students.add(student);
+		
+		return students;
+	}
+	
+	@PutMapping("/students/{roll}")
+	public Student updateStudent(@RequestBody Student student, @PathVariable Integer roll) {
+		
+		boolean flag = false;
+		
+		for(Student s1 : students) {
+			
+			if(s1.getRoll()==roll) {
+				s1.setName(student.getName());
+				s1.setMarks(student.getMarks());
+
+				flag=true;
+				break;
+			}
+			
+		}
+		if(!flag) {
+			throw new IllegalArgumentException("Student not found with roll"+roll);
+		}
+		
+		return student;
+	}
+	
+	@DeleteMapping("/students/{roll}")
+	public String deleteStudentByRollhandler(@PathVariable("roll") Integer roll) {
+		
+		boolean s1 = students.removeIf(s->s.getRoll()==roll);
+		
+		if(s1) {
+			return "student removed succesfully..!";
+		}
+		else
+			throw new IllegalArgumentException("no Student found with that roll number"+roll);
+	}
+	
+	@PutMapping("/studentsgrace/{roll}")
+	public String graceMarks(@PathVariable("roll") Integer roll , @RequestParam("gmarks") Integer gmarks) {
+		boolean flag = false;
+		
+		for(Student s1 : students) {
+			
+			if(s1.getRoll()==roll) {
+				s1.setMarks((s1.getMarks())+gmarks);
+
+				flag=true;
+				break;
+			}
+			
+		}
+		if(!flag) {
+			throw new IllegalArgumentException("Student not found with roll"+roll);
+		}
+	
+		return "student marks graced";
+	}
+}
