68548f93007f0bd2eba532c7ab323da5c9e18669
unknown <rajatsantra8116@gmail.com>
2022-12-08 23:59:33 +0530

    test

--- Day-10Ass/src/test/java/com/masai/Day10AssApplicationTests.java
+++ Day-10Ass/src/test/java/com/masai/Day10AssApplicationTests.java
@@ -1,13 +1,27 @@
 package com.masai;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
+import com.masai.repo.EmployeeRepo;
+import com.masai.service.EmpService;
+
+//@RunWith()
 @SpringBootTest
 class Day10AssApplicationTests {
 
+	@Autowired
+	private EmpService eservice;
+	
+	@MockBean
+	private EmployeeRepo erepo;
+	
 	@Test
-	void contextLoads() {
+	public void FindEmployeeById() {
+		Integer empId =1;
+		
 	}
 
 }
